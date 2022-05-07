915d35f0f45e064bb4713dc69daf111b2aff254b
Richard Clarke <freshnewrich@gmail.com>
2022-05-07 16:38:38 -0700

    envs, useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Display from './components/Display';
+// import React from 'react'
+import {useState} from 'react'
 
 function App() {
+  //api key const
+const KEY = process.env.REACT_APP_API_KEY
+const [gif, setGif] = useState(null)
+
   return (
     <div className="App">
       <h1>React Giphy API Project</h1>
       <div><button>GET A GIF</button></div>
-      <Display />
+      <Display gif={gif}/>
     </div>
   );
 }
